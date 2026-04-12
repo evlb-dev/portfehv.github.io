fbd4232d17ef21adc98484a6c7f388286753b686
Eugenia <evillalba@icop.edu.ar>
2026-04-11 21:39:43 -0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,10 @@
-import { defineConfig, envField } from "astro/config";
+import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import vercel from "@astrojs/vercel";
 
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
-  },
-  env: {
-    schema: {
-      RESEND_API_KEY: envField.string({ context: "server", access: "secret" }),
-      MY_EMAIL: envField.string({ context: "server", access: "secret" }),
-    },
   },
   output: "server",
   adapter: vercel(),
