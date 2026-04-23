caba1ebdceb6f73b361e3da4848438f49f589c70
Eugenia <evillalba@icop.edu.ar>
2026-04-23 20:22:23 -0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 import vercel from "@astrojs/vercel";
 
 export default defineConfig({
+  site:https://portfehv.github.io
   vite: {
     plugins: [tailwindcss()],
   },
