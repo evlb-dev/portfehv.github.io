85acbfa0b36914b4cbeaa82115a4a903f823a939
evlb-dev <evillalba@icop.edu.ar>
2026-05-07 20:22:56 -0300

    setup vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
-
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
-  site: 'https://evlb-dev.github.io',
-  base: '/portfehv.github.io',
-  output: 'static'
+  adapter: vercel(),
 });
