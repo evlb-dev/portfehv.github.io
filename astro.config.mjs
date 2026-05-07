ec42bc30a88147d4526d0f8f87eb57d229f82fb4
evlb-dev <evillalba@icop.edu.ar>
2026-05-07 20:43:55 -0300

    fix tailwind integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
+import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   adapter: vercel(),
+  integrations: [tailwind()],
 });
